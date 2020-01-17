31da90aba7824c03bb03bf268d9b0d3a4aef7aa1
Prakash King <ptan022@gmail.com>
2020-01-17 13:14:55 +0530

    new file added

---
+++ mangobite/smax.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math
+
+# Write a function that takes as input a list of numbers, and returns
+# the list of values given by the softmax function.
+def softmax(L):
+    expL = np.exp(L)
+    sumExpL = sum(expL)
+    result = []
+    for i in expL:
+        result.append(i * 1.0 / sumExpL)
+    return result
+
+# call function for input
+L = [2,1,0]
+print(softmax(L))
